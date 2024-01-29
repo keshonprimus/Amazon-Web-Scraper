921d19c93f5318784bb5435d68fcd1c575482cc0
keshon primus <90883812+keshonprimus@users.noreply.github.com>
2024-01-29 19:57:10 -0400

    Initial Commit - Provisioning of Webscraper to retreive product title and image.

---
+++ amazon_webscraper.mjs
@@ -0,0 +1,23 @@
+import puppeteer from 'puppeteer'
+
+// Scrape Amazon Webpage
+export async function ScrapeAmazon(url){    
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url)
+
+    // Element image
+    const [elimg] = await page.$x('//*[@id="landingImage"]');
+    const elsrc = await elimg.getProperty('src');
+    const elimgvalue = await elsrc.jsonValue();
+
+    //Element title
+    const [eltitle] = await page.$x('//*[@id="productTitle"]')
+    const eltitletxt = await eltitle.getProperty('textContent')
+    const eltitlerawtxt = await eltitletxt.jsonValue();
+
+    await(browser.close())
+    
+    return [eltitlerawtxt, elimgvalue, elpricerawtxt, elavailableclean, url]
+}
+
